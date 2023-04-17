cba67b302363ff1875177c11c05092c3f4e17991
Lucas Hemmingsen <99349701+LucasHemm@users.noreply.github.com>
2023-04-17 12:55:03 +0200

    jokes rest assured test added

--- src/main/java/rest/UserResource.java
+++ src/main/java/rest/UserResource.java
@@ -1,28 +1,31 @@
 package rest;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dtos.RoleDTO;
+import dtos.UserDTO;
+import entities.Role;
 import entities.User;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.annotation.security.RolesAllowed;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.TypedQuery;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.Produces;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+
+import facades.FacadeExample;
+import facades.UserFacade;
 import utils.EMF_Creator;
 
-/**
- * @author lam@cphbusiness.dk
- */
 @Path("info")
 public class UserResource {
     
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private static final UserFacade FACADE =  UserFacade.getUserFacade(EMF);
+
     @Context
     private UriInfo context;
 
@@ -68,4 +71,30 @@ public class UserResource {
         String thisuser = securityContext.getUserPrincipal().getName();
         return "{\"msg\": \"Hello to (admin) User: " + thisuser + "\"}";
     }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("create")
+    public Response createUser(String input){
+        UserDTO udto = GSON.fromJson(input, UserDTO.class);
+        List<Role> roleList = udto.getRoleList().stream().map(r -> new Role(r.getRoleName()) ).collect(Collectors.toList());
+        User user = new User(udto.getUserName(), udto.getUserPass(),roleList);
+        UserDTO udtoNew = FACADE.create(user);
+        return Response.ok().entity(udtoNew).build();
+    }
+    @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("delete/{username}")
+    public Response deleteUser(@PathParam("username") String username){
+        UserDTO udto = FACADE.delete(username);
+        return Response.ok().entity(udto).build();
+    }
+
+
+
+
+
+
+
 }
